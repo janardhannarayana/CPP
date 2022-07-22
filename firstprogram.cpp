5d612c60948caefa24c9290827b338884f1bf545
janardhannarayana <janardhannarayana@gmail.com>
2022-07-22 16:03:10 +0530

    Create firstprogram.cpp

---
+++ firstprogram.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Header files containing the input and output stream objects.
+using namespace std;  // namespace std has cout and cin objects defined.
+
+int main()
+{
+   cout << "Hello World!";
+   cout << "Hello World!" << endl; //endl is for printing new line
+
+   return 0;
+}
